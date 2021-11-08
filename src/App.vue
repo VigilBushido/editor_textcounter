58a95959982c55f35c0f7409b2ded7d8dc10fccb
VigilBushido <vigilbushido@gmail.com>
2021-11-08 11:04:38 -0500

    small change

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <EditorWordCounter />
+    <MessageEditor />
   </div>
 </template>
 
 <script>
 import EditorWordCounter from "./components/EditorWordCounter";
+import MessageEditor from "./components/MessageEditor";
 
 export default {
   name: "App",
   components: {
     EditorWordCounter,
+    MessageEditor,
   },
 };
 </script>

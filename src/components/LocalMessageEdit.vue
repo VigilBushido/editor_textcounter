55a7883ce512adbb0b8219676d13141aeb1a6432
VigilBushido <vigilbushido@gmail.com>
2021-11-09 23:50:10 -0500

    LocalMessage view added

---
+++ src/components/LocalMessageEdit.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <textarea ref="textArea" @change="onChange($event)"> </textarea>
+    <button @click="onSendClick()">Send</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>

6b77b10c7e58bf4be4ab9c487d71e548ac67d43f
VigilBushido <vigilbushido@gmail.com>
2021-11-10 21:23:03 -0500

    corejs issue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <EditorWordCounter />
-    <p>Mssage: {{ message }}</p>
-    <MessageEditor @send="message = $event" />
-    <button @click="message = null">Reset</button>
+    <!-- <EditorWordCounter />
+    <p>Message: {{ message }}</p> -->
+    <!-- <MessageEditor @send="message = $event" />
+    <button @click="message = null">Reset</button>  -->
     <!--Activity 4.01-->
     <MessageFeed :messages="messages" />
     <LocalMessageEdit @send="onSend($event)" />
@@ -11,22 +11,24 @@
 </template>
 
 <script>
-import EditorWordCounter from "./components/EditorWordCounter";
-import MessageEditor from "./components/MessageEditor";
+//import EditorWordCounter from "./components/EditorWordCounter";
+//import MessageEditor from "./components/MessageEditor";
 import LocalMessageEdit from ".components/LocalMessageEdit";
 import MessageFeed from ".components/MessageFeed";
+import "core-js/es/array";
 
 export default {
   name: "App",
   components: {
-    EditorWordCounter,
-    MessageEditor,
+    //EditorWordCounter,
+    //MessageEditor,
     LocalMessageEdit,
     MessageFeed,
   },
   data() {
     return {
-      message: null,
+      //message: null, <- the version used for MessageEditor
+      messages: [],
     };
   },
   methods: {
--- src/components/MessageFeed.vue
+++ src/components/MessageFeed.vue
@@ -7,6 +7,8 @@
 </template>
 
 <script>
+import "core-js/es/array";
+
 export default {
   props: {
     messages: {
